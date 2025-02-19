f1290b26904cfbc99538af7c5ed623c0565d2ff9
SocaDLV <ivan.planero@gmail.com>
2025-02-19 21:55:24 +0200

    ultimIntentoONNX

--- Fine-Tuning-MNV2_v3_cifar10/convertToONNX_v2.py
+++ Fine-Tuning-MNV2_v3_cifar10/convertToONNX_v2.py
@@ -1,32 +1,36 @@
 import tensorflow as tf
-import tf2onnx
 import onnx
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-# Cargar el modelo H5
+# Ruta del modelo Keras (.h5)
 model_path = r'C:\Users\Ivan\Desktop\Asignatures5tcarrera\TFG\codi\Fine-Tuning-MNV2_v3_cifar10\modelosFTuneados\mnv2_cifar10_bo_v1.h5'
 model = tf.keras.models.load_model(model_path)
 
-# Crear un concrete function con batch size = 1 para cada entrada
-concrete_inputs = [
-    tf.TensorSpec([1] + list(model_input.shape)[1:], model_input.dtype)
-    for model_input in model.inputs
-]
-# Define la función concreta
+# Crear un concrete function con batch size = 1
+input_shape = (1, 224, 224, 3)  # Asegúrate de que este sea el tamaño correcto de entrada
+input_signature = tf.TensorSpec(shape=input_shape, dtype=tf.float32, name="input")
+
+# Definir la función concreta
 full_model = tf.function(lambda inputs: model(inputs))
-concrete_function = full_model.get_concrete_function(concrete_inputs)
+concrete_function = full_model.get_concrete_function(input_signature)
 
-# Imprimir nombres de entradas y salidas (para ver que se fija correctamente la forma)
-input_names = [inp.name for inp in concrete_function.inputs]
-output_names = [out.name for out in concrete_function.outputs]
-print("Inputs:", input_names)
-print("Outputs:", output_names)
-
-# Convertir variables a constantes (congelar el grafo)
+# Congelar el modelo
 frozen_func = convert_variables_to_constants_v2(concrete_function)
 frozen_graph_def = frozen_func.graph.as_graph_def()
 
-# Utilizar tf2onnx para procesar el grafo congelado
+# Crear un grafo de TensorFlow a partir del modelo congelado
+with tf.Graph().as_default() as tf_graph:
+    tf.import_graph_def(frozen_graph_def, name='')
+
+# Obtener los nombres de las entradas y salidas del modelo congelado
+input_names = [frozen_func.inputs[0].name]  # Nombre del primer tensor de entrada
+output_names = [frozen_func.outputs[0].name]  # Nombre del primer tensor de salida
+
+# Mostrar los nombres de entrada y salida
+print("Input names:", input_names)
+print("Output names:", output_names)
+
+# Procesar el grafo para convertirlo a ONNX
 from tf2onnx import tf_loader
 from tf2onnx.tfonnx import process_tf_graph
 from tf2onnx.optimizer import optimize_graph
@@ -35,21 +39,13 @@ from tf2onnx import utils, constants
 # Extra opset para contrib (si es necesario)
 extra_opset = [utils.make_opsetid(constants.CONTRIB_OPS_DOMAIN, 1)]
 
-with tf.Graph().as_default() as tf_graph:
-    tf.import_graph_def(frozen_graph_def, name='')
-
 with tf_loader.tf_session(graph=tf_graph) as sess:
-    onnx_graph = process_tf_graph(
-        tf_graph, 
-        input_names=input_names, 
-        output_names=output_names, 
-        extra_opset=extra_opset
-    )
+    onnx_graph = process_tf_graph(tf_graph, input_names=input_names, output_names=output_names, extra_opset=extra_opset)
 
 # Optimizar el grafo ONNX
 onnx_graph = optimize_graph(onnx_graph)
 model_proto = onnx_graph.make_model("converted")
 
 # Guardar el modelo ONNX resultante
-utils.save_protobuf("model2b.onnx", model_proto)
-print("Conversion complete!")
+onnx.save_model(model_proto, "model2b.onnx")
+print("Conversión a ONNX completada!")
