2c9b8e300c7b9af7b7e46321247146810e1d6074
SocaDLV <ivan.planero@gmail.com>
2025-02-23 19:13:40 +0200

    preparemArxiuPaConvertirPytorchAGPUOnnx

--- Fine-Tuning-ResNet_cifar10/convertToONNX.py
+++ Fine-Tuning-ResNet_cifar10/convertToONNX.py
@@ -3,7 +3,7 @@ import torch
 
 model_path = r'C:\Users\Ivan\Desktop\Asignatures5tcarrera\TFG\codi\Fine-Tuning-ResNet_cifar10\modelsPytorch\stage-2-pytorch'
 # Cargar el modelo usando torch.load()
-model = torch.load(model_path, map_location=torch.device('cpu'))
+model = torch.load(model_path, map_location=torch.device('cuda'))
 
 # 1. Export to ONNX
-torch.onnx.export(model, (torch.rand(1, 3, 32, 32), ), 'stage-2.onnx')
+torch.onnx.export(model, (torch.rand(1, 3, 32, 32), ), 'stage-2-cuda.onnx')
