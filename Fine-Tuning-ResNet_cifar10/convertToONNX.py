4c50818844c00b90beed7b0ea1c1545ffad5cfea
SocaDLV <ivan.planero@gmail.com>
2025-02-23 20:29:21 +0200

    Uns quants Canvis

--- Fine-Tuning-ResNet_cifar10/convertToONNX.py
+++ Fine-Tuning-ResNet_cifar10/convertToONNX.py
@@ -1,7 +1,11 @@
 import torchvision
 import torch
+import os
 
-model_path = r'C:\Users\Ivan\Desktop\Asignatures5tcarrera\TFG\codi\Fine-Tuning-ResNet_cifar10\modelsPytorch\stage-2-pytorch'
+#model_path = r'C:\Users\Ivan\Desktop\Asignatures5tcarrera\TFG\codi\Fine-Tuning-ResNet_cifar10\modelsPytorch\stage-2-pytorch'
+
+model_path = os.path.expanduser('~/codi/TFG/Fine-Tuning-ResNet_cifar10/modelsPytorch/stage-2-pytorch')
+
 # Cargar el modelo usando torch.load()
 model = torch.load(model_path, map_location=torch.device('cuda'))
 
