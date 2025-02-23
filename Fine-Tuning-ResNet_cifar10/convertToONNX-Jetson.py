141b40e1bbfb59a0110b0571df3bc9f5e93f4f48
SocaDLV <ivan.planero@gmail.com>
2025-02-23 20:34:56 +0200

    nouModelOnnxJetsonResNetCifar10

--- Fine-Tuning-ResNet_cifar10/convertToONNX-Jetson.py
+++ Fine-Tuning-ResNet_cifar10/convertToONNX-Jetson.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 # Ruta del modelo
-model_path = os.path.expanduser('~/codi/TFG/Fine-Tuning-ResNet_cifar10/modelsPytorch/stage-2-pytorch')
+model_path = r'C:\Users\Ivan\Desktop\Asignatures5tcarrera\TFG\codi\Fine-Tuning-ResNet_cifar10\modelsPytorch\stage-2-pytorch'
 
 # Cargar el modelo usando torch.load()
 model = torch.load(model_path, map_location=torch.device('cpu'))
